85d597b35f8d01a3956d47818c1489fb2d47a104
PhuongPhan <phuongtp@gmail.com>
2021-03-04 14:50:31 +0100

    End of Session 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,7 @@ namespace API
       // });
 
       services.AddControllers();
+      services.AddCors();
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -61,6 +62,7 @@ namespace API
 
       app.UseRouting();
 
+      app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints =>
